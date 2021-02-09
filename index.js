8aed23a207ccbb1603608993f8b43d2382d8fa3e
antiremy <antiremy@users.noreply.github.com>
2021-02-08 21:54:54 -0600

    Add code to check nicehash, change frontend

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const cote = require('cote')
 const express = require('express')
 const app = express();
 const http = require('http').createServer(app);
+const axios = require('axios')
+const config = require('./config.json')
 const io = require('socket.io')(http, {
     cors: {
-        origin: "http://localhost:23465",
+        origin: "*",
         methods: ["GET", "POST"]
     }
 });
 
-app.use('/', express.static('build'))
+app.use('/', express.static(__dirname + '/build'))
+
+var nicehashData = {}
 
 const monitor = new cote.Monitor({
     name: 'hardware monitor',
@@ -19,14 +23,22 @@ var sockend
 
 io.on('connection', socket => {
     console.log(socket.id)
+    socket.emit('nicehash', nicehashData)
 })
 
+if (config.walletAddress){getNicehash(); setInterval(getNicehash, 30000)}
 
+async function getNicehash() {
+    var response = await axios(`https://api2.nicehash.com/main/api/v2/mining/external/${config.walletAddress}/rigs2`)
+    nicehashData = response.data
+    io.emit('nicehash', nicehashData)
+}
 
 function processNodes(nodes) {
     for (let id in nodes) {
         var node = nodes[id]
-        if (typeof sockend === 'undefined' && node.advertisement.name === 'hardware monitor' && node.isMaster) {
+        console.log(node)
+        if (typeof sockend === 'undefined' && node.advertisement.type === 'service' && node.advertisement.name === 'hardware monitor' && node.isMaster) {
             console.log('Found master node', node)
             sockend = new cote.Sockend(io, {
                 name: 'hardware monitor',
