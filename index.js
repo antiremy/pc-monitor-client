e3e9b965ad314354c6b99a5fb603ef8a388104c4
antiremy <antiremy@users.noreply.github.com>
2021-02-09 16:30:41 -0600

    Change frontend, add error handling

--- index.js
+++ index.js
@@ -29,15 +29,21 @@ io.on('connection', socket => {
 if (config.walletAddress){getNicehash(); setInterval(getNicehash, 30000)}
 
 async function getNicehash() {
-    var response = await axios(`https://api2.nicehash.com/main/api/v2/mining/external/${config.walletAddress}/rigs2`)
-    nicehashData = response.data
-    io.emit('nicehash', nicehashData)
+    try {
+        var response = await axios(`https://api2.nicehash.com/main/api/v2/mining/external/${config.walletAddress}/rigs2`)
+        var {data: prices} = await axios(`https://api2.nicehash.com/exchange/api/v2/info/prices`)
+        nicehashData = {...response.data, prices, ts: new Date()}
+        io.emit('nicehash', nicehashData)
+    }
+    catch(e) {
+        console.log('Error getting Nicehash data', e.toString())
+    }
+
 }
 
 function processNodes(nodes) {
     for (let id in nodes) {
         var node = nodes[id]
-        console.log(node)
         if (typeof sockend === 'undefined' && node.advertisement.type === 'service' && node.advertisement.name === 'hardware monitor' && node.isMaster) {
             console.log('Found master node', node)
             sockend = new cote.Sockend(io, {
